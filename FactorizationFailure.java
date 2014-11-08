2dfdc0e39d5df7e41605865345d982e060b86142
JonathanMurray <jonte_murray@hotmail.com>
2014-11-08 15:03:59 +0100

    Added a naive pollardRho-method

---
+++ FactorizationFailure.java
@@ -0,0 +1,10 @@
+public class FactorizationFailure extends Exception{
+
+	public FactorizationFailure(){
+		super();
+	}
+
+	public FactorizationFailure(String msg){
+		super(msg);
+	}
+}
--- Main.java
+++ Main.java
@@ -2,15 +2,17 @@ import java.math.*;
 
 public class Main {
 
-	public Main() {
-		BigInteger a = BigInteger.valueOf(10);
-		BigInteger b = BigInteger.valueOf(12);
-
-		System.out.println("gcd(" + a + ", " + b + ") = " + gcd(a, b));
+	public Main() {	
 	}
 
-	public static void main(String[] args) {
-		new Main();
+	public static void main(String[] args) throws FactorizationFailure{
+		Main m = new Main();
+		BigInteger a = BigInteger.valueOf(10);
+		BigInteger b = BigInteger.valueOf(12);
+		BigInteger c = BigInteger.valueOf(1002);
+
+		System.out.println("gcd(" + a + ", " + b + ") = " + m.gcd(a, b));
+		System.out.println("pollardRho(" + c + ") = " + m.pollardRho(c));
 	}
 
 	public BigInteger gcd(BigInteger a, BigInteger b){
@@ -22,4 +24,25 @@ public class Main {
 		}
 		return a;
 	}
+
+	public BigInteger pollardRho(BigInteger n) throws FactorizationFailure{
+		BigInteger x = BigInteger.valueOf(2);
+		BigInteger y = BigInteger.valueOf(2);
+		BigInteger d = BigInteger.ONE;
+		while(d == BigInteger.ONE){
+			x = g(x, n);
+			y = g(g(y, n), n);
+			d = gcd(x.subtract(y).abs(), n);
+		}
+		if(d == n){
+			throw new FactorizationFailure();
+		}
+		return d;
+	}
+
+	// Function used by pollardRho
+	public BigInteger g(BigInteger x, BigInteger n){
+		return x.pow(2).add(BigInteger.ONE).mod(n);
+	}
+
 }
