dee9c093061c84f84a36cf160efd3b6a27183d80
JonathanMurray <jonte_murray@hotmail.com>
2014-11-08 14:57:10 +0100

    Added gcd-method

--- Main.java
+++ Main.java
@@ -1,10 +1,25 @@
+import java.math.*;
+
 public class Main {
 
 	public Main() {
-		System.out.println("Hellloooooo world!!!");
+		BigInteger a = BigInteger.valueOf(10);
+		BigInteger b = BigInteger.valueOf(12);
+
+		System.out.println("gcd(" + a + ", " + b + ") = " + gcd(a, b));
 	}
 
 	public static void main(String[] args) {
 		new Main();
+	}
+
+	public BigInteger gcd(BigInteger a, BigInteger b){
+		BigInteger t;
+		while(b != BigInteger.ZERO){
+			t = b;
+			b = a.mod(b);
+			a = t;
+		}
+		return a;
 	}
 }
