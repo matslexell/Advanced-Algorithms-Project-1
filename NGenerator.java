9973eb588bca0015daf10e3340d4a0debff57dde
matselexell <matslexell88@gmail.com>
2014-11-19 08:15:14 +0100

    Made some test code in NGenerator
    Added functions for generating small and tiny (even smaller) integers to factorize

--- NGenerator.java
+++ NGenerator.java
@@ -20,8 +20,42 @@ public class NGenerator {
 
 		for (int i = 1; i <= 100; i++)
 			array.add(equation(jonathan, i));
-		
+
 		return array;
+	}
+
+	public static ArrayList<BigInteger> getSmallNumbers() {
+		ArrayList<BigInteger> array = new ArrayList<BigInteger>();
+
+		for (int i = 1; i <= 100; i++)
+			array.add(equationSmall(mats, i));
+
+		for (int i = 1; i <= 100; i++)
+			array.add(equationSmall(jonathan, i));
+
+		return array;
+	}
+	
+	public static ArrayList<BigInteger> getTinyNumbers() {
+		ArrayList<BigInteger> array = new ArrayList<BigInteger>();
+
+		for (int i = 1; i <= 100; i++)
+			array.add(equationTiny(mats, i));
+
+		for (int i = 1; i <= 100; i++)
+			array.add(equationTiny(jonathan, i));
+
+		return array;
+	}
+	
+	private static BigInteger equationTiny(long P, int i) {
+		BigInteger big = BigInteger.valueOf(10).pow((int) (5 + j));
+		return BigInteger.valueOf(P).multiply(big).add(BigInteger.valueOf(i));
+	}
+	
+	private static BigInteger equationSmall(long P, int i) {
+		BigInteger big = BigInteger.valueOf(10).pow((int) (12 + j));
+		return BigInteger.valueOf(P).multiply(big).add(BigInteger.valueOf(i));
 	}
 
 	private static BigInteger equation(long P, int i) {
@@ -29,4 +63,6 @@ public class NGenerator {
 		return BigInteger.valueOf(P).multiply(big).add(BigInteger.valueOf(i));
 	}
 
+	
+	
 }
